d617b04c9b25460745553b2f743cfa06bdb10326
Vedant Chainani <vedantchainani1084@gmail.com>
2023-06-13 17:44:45 +0530

    add initial join page

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -1,14 +1,56 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { Input, Button } from '@nextui-org/react';
+import { ArrowRight } from 'react-iconly';
 import { Sidebar, NavBar } from '@/components';
+import Header from '@/components/layout/headers/join-meeting';
 
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 const Join = () => {
+	const [meetingId, setMeetingId] = React.useState<string>('');
+	const router = useRouter();
+
+	const handleJoinMeeting = () => {
+		if (!meetingId) {
+			toast.error('Meeting ID cannot be empty');
+			return;
+		}
+		router.push(`/join/${meetingId}`);
+	};
 	return (
 		<main className={`${inter.className}`}>
 			<NavBar />
-			<div className='flex flex-row justify-between'>
+			<div className='flex flex-row justify-start'>
 				<Sidebar />
+				<div className='w-full'>
+					<Header />
+					<div className='flex flex-col gap-8 items-center mt-16'>
+						<Input
+							placeholder='Meeting ID'
+							size='xl'
+							clearable
+							className='mt-4 max-w-[450px]'
+							onChange={(e) => setMeetingId(e.target.value)}
+						/>
+						<Button
+							auto
+							light
+							iconRight={
+								<ArrowRight set='bold' primaryColor='#fff' size={32} />
+							}
+							size='lg'
+							className='bg-[#0072F5] text-white mt-4 !w-fit'
+							onPress={() => handleJoinMeeting()}
+						>
+							Join
+						</Button>
+					</div>
+				</div>
+				<Toaster position='bottom-left' />
 			</div>
 		</main>
 	);
